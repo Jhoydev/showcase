93d12c4b25afce673a7e983411c65bc688756493
Jhoseph <jhoseph@insignia.com.co>
2020-06-13 01:20:26 +0200

    init

--- index.php
+++ index.php
@@ -6,8 +6,19 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Jhoseph</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+    <style>
+        body{
+            font-family: 'Roboto', sans-serif;
+        }
+        h1{
+            font-size: 50px;
+        }
+    </style>
 </head>
 <body>
-<h1>JHOSEPH.DEV</h1>
+    <div style="width: 100%; height: 100vh; display: flex; justify-content: center; align-items: center">
+        <h1>JHOSEPH.DEV</h1>
+    </div>
 </body>
 </html>
