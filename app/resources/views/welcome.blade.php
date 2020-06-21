2c057b3d8cc0185eb64b607a39b3380931faa955
Jhoseph <jhoseph@insignia.com.co>
2020-06-21 02:51:30 +0200

    favicon

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Jhoseph.Dev</title>
+        <link rel="icon" class="js-site-favicon" type="image/svg+xml" href="/favicon.svg">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
