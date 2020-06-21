0db08d8a14c31cde9fba9f7c7ae3cc5f45b436eb
Jhoseph <jhoseph.dev@gmail.com>
2020-06-21 02:58:59 +0200

    favicon

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -11,6 +11,7 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@500&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
         <!-- Styles -->
         <style>
             *{
